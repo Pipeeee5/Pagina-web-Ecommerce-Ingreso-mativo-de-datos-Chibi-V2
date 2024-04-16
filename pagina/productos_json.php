1dc1b29bc9311efb8686f8b4163ee4788e4215c2
ChubiLanes <134746792+Chubi12@users.noreply.github.com>
2024-04-15 20:00:55 -0400

    formato_json
    subi el archibo del profe para empezar a trabajar con json

---
+++ pagina/productos_json.php
@@ -0,0 +1,42 @@
+<?php
+require 'config/config.php';
+require 'config/database.php';
+$db = new Database();
+$con = $db->conectar();
+
+$sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
+$sql->execute();
+$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+//session_destroy();
+
+//print_r($_SESSION);
+?>
+
+<?php 
+   header('Content-type: application/json');
+   header('Content-harrys: application/json');
+//    var_dump($resultado);
+//    echo '[';
+//    $comma = "";
+//    foreach ($resultado as $row) {
+//     echo $comma . '{';
+//     echo '"id": "' . $row["id"] . '",';
+//     echo '"nombre": "' . $row["nombre"] . '",';
+//     echo '"precio": "' . $row["precio"] . '"';
+//     echo '}';
+//     $comma = ",";
+    
+//  } 
+//  echo ']';
+
+ echo json_encode( $resultado );
+ ?>
+
+
+
+      
+
+
